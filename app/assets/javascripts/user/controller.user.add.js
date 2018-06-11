ba7884ccd3257929bc17115c6111de60009604ca
erickwong <eric1234463@gmail.com>
2018-06-11 08:34:53 +0000

    feat(User) -- add user directive.js

---
+++ app/assets/javascripts/user/controller.user.add.js
@@ -0,0 +1,26 @@
+app.controller("UserEditController", [
+  "$scope",
+  "$location",
+  "$routeParams",
+  "UserService",
+  function($scope, $location, $routeParams, UserService) {
+    $scope.user = {
+      age: null,
+      gender: null,
+      first_name: "",
+      last_name: "",
+      address: {
+        country: "",
+        address_1: "",
+        address_2: ""
+      }
+    };
+
+    $scope.handleFormSubmit = function() {
+      const user = $scope.user;
+      UserService.createUser(user).then(function(response) {
+        $location.path("/users");
+      });
+    };
+  }
+]);
---
+++ app/assets/javascripts/user/user-form.directive.js
@@ -0,0 +1,5 @@
+app.directive("userForm", function() {
+  return {
+    templateUrl: "/views/users/userForm.html"
+  };
+});
